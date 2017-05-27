bce985fd58138f88df85287a7f3364c294e34e6d
Andrew Bedford-Hart <abedfordhart@gmail.com>
2017-05-27 11:46:27 -0700

    Implement getOneGuide method

--- server/controllers/guides.js
+++ server/controllers/guides.js
@@ -3,12 +3,12 @@ const models = require('../../db/models');
 module.exports.createGuide = (req, res) => {
   models.User.where({username: req.body.username}).fetch({columns: ['id']})
    .then(result => {
-    models.Guide.forge({ user_id: result.id, city: req.body.city, hourly_rate: req.body.hourlyRate, intro: req.body.intro, statement: req.body.statement})
+     models.Guide.forge({ user_id: result.id, city: req.body.city, hourly_rate: req.body.hourlyRate, intro: req.body.intro, statement: req.body.statement})
       .save()
       .then(result => {
         console.log('success creating guide!!');
         res.status(200).send();
-      })
+      });
    })
     .error(err => {
       res.status(500).send(err);
@@ -18,8 +18,9 @@ module.exports.createGuide = (req, res) => {
     });   
 };
 
-module.exports.getUser = (req, res) => {
-  models.User.where({ username: req.params.username}).fetch()
+module.exports.getOneGuide = (req, res) => {
+  console.log(req.params)
+  models.Guide.where({id: req.params.id}).fetch()
     .then(profile => {
       if (!profile) {
         throw profile;
--- server/routes/guides.js
+++ server/routes/guides.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const GuideController = require('../controllers').Guides;
 
 router.route('/')
-  // .get(UserController.getAll)
   .post(GuideController.createGuide)
   ;
 
 router.route('/:id')
+  .get(GuideController.getOneGuide)
   // .get(GuideController.getGuide)
   // .put(UserController.update)
   // .delete(UserController.deleteOne)
