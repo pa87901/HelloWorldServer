4509e60f9008a6c1bb936a4cc677c4fa9694ab20
Alexander Liang <axl.lgn@gmail.com>
2017-05-29 22:37:27 -0700

    Working authentication middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,13 +1,27 @@
-// const session = require('express-session');
-// const RedisStore = require('connect-redis')(session);
-// const redisClient = require('redis').createClient();
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const redisClient = require('redis').createClient();
+const JWT = require('jwt-async');
 
-// module.exports.verify = (req, res, next) => {
-//   if (req.isAuthenticated()) {
-//     return next();
-//   }
-//   res.redirect('/login');
-// };
+var jwt = new JWT();
+jwt.setSecret('wfxy2qVrpjMzh0D2hIzImcB_iOLH97Eiq6yyvwWI6jyc6Q8M8RFyHnqTTKaL6_hK');
+
+module.exports.verify = function verify(req, res, next) {
+  req.jwt_auth = false;
+  if(req.headers['authorization']){
+    var jwt_token = req.headers['authorization'];
+    jwt.verify(jwt_token, function(err, jwt_data) {
+      if(err){
+        throw err;
+      } else {
+        req.jwt_auth = jwt_data;
+        next();
+      }
+    });
+  } else {
+    next();
+  }
+};
 
 // module.exports.session = session({
 //   store: new RedisStore({
