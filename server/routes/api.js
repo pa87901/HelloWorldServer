d3fe85e5b1608cd266befa78637dde16a0f6bc13
Alexander Liang <axl.lgn@gmail.com>
2017-05-31 17:45:13 -0700

    test

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,9 +4,11 @@ const router = express.Router();
 
 router.route('/')
   .get((req, res) => {
+    console.log(req)
     res.status(200).send('Hello World!');
   })
   .post((req, res) => {
+    console.log(req)
     console.log('in the correct route');
     res.status(201).send({ data: 'Posted!' });
   });
