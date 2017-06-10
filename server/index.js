9c77a1658b169f90efebc3fc6c0163a17492461b
Preda Anuvatnujotikul <preda.anu@gmail>
2017-06-10 10:29:07 -0700

    Implemented socket chat better

--- server/index.js
+++ server/index.js
@@ -62,11 +62,17 @@ io.on('connection', (socket) => {
     };
     chatsController.getChat(req2, null, (data2) => {
       // Combine the chats.
+      if (!data.models.length) {
+        data = {models: []};
+      }
+      if (!data2.models.length) {
+        data2 = {models: []};
+      }
       data = data.models.concat(data2.models);
       // data.sort((a, b) => {
       //   return a.
       // })
-      // console.log('DATA', data, data.length);
+      console.log('DATA', data, data.length);
       io.emit('chat message', JSON.parse(JSON.stringify(data)));
     });
   });
