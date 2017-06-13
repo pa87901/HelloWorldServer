58dff205ef81b671addb4d90a38f49d1bc5cbb93
Charles Kim <cjkim0119@gmail.com>
2017-06-12 19:21:40 -0700

    (heroku) adjust port

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = require('./app');
 const http = require('http').Server(app);
 module.exports.http = http;
 const db = require('../db');
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const workers = require('./workers');
 var CronJob = require('cron').CronJob;
 
