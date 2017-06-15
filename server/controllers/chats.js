d9939b3eeea9401555343fdff2089a3265c59422
Preda Anuvatnujotikul <preda.anu@gmail.com>
2017-06-14 20:03:05 -0700

    Reconfigured chat methods to enable chat to work.

--- server/controllers/chats.js
+++ server/controllers/chats.js
@@ -29,53 +29,6 @@ module.exports.createChat = (req, res) => {
     });
 };
 
-// module.exports.getChat = (req, res, callback) => {
-//   console.log('REQ PARAMS', req.params);
-//   models.User.where({facebook_id: req.params.facebookId}).fetch({columns: ['id']})
-//   .then(result => {
-//     models.User.where({id: req.params.guideFacebookId}).fetch({columns: ['id']})
-//     .then(result2 => {
-//       models.Guide.where({user_id: result2.id}).fetch({columns: ['id']})
-//       .then(result3 => {
-//         models.Chat.query((qb) => {
-//           qb.limit(100); 
-//           qb.orderBy('created_at', 'desc');
-//         })
-//           .where({user_id: result.id, guide_id: result3.id}).fetchAll({
-//             withRelated: [
-//               {
-//                 'user': (qb) => {
-//                   qb.select();
-//                 }
-//               },
-//               {
-//                 'guide.user': (qb) => {
-//                   qb.select();
-//                 }
-//               }
-//             ]
-//           })
-//           .then(chats => {
-//             if (!chats) {
-//               throw chats;
-//             }
-//             if (res) {
-//               res.status(200).send(chats);
-//             } else {
-//               callback(chats);
-//             }
-//             console.log('Successfully fetched chats!!');
-//           });
-//       });
-//     });
-//   })
-//     .error(err => {
-//       res.status(500).send(err);
-//     })
-//     .catch(() => {
-//       res.status(404).send([]);
-//     });
-// };
 
 
 module.exports.getChat = (req, res, callback) => {
@@ -84,13 +37,7 @@ module.exports.getChat = (req, res, callback) => {
   .then(user1 => {
     models.User.where({facebook_id: req.params.guideFacebookId}).fetch({columns: ['id']})
     .then(user2 => {
-      // console.log(result.id, result2.id)
-      // models.Guide.where({user_id: result2.id}).fetch({columns: ['id']})
-      // .then(result3 => {
       models.Chat.query(qb => {
-        // userId = result.id;
-        // userId2 = result2.id;
-        console.log("Result1", user1);
         qb.limit(100);
         qb.orderBy('created_at', 'desc');
         qb.where({user_id: user1.id})
@@ -103,30 +50,20 @@ module.exports.getChat = (req, res, callback) => {
       })
       .then(chats => {
         chats = JSON.parse(JSON.stringify(chats));
-        // chats = chats.filter(chat=> {
-        //   if ((chat.user_id === user1.id && chat.guide_id === user2.id) ||
-        //     (chat.user_id === user2.id && chat.guide_id === user1.id)) {
-        //     return true;
-        //   }
-        //   return false;
-        // });
-
-        var promiseArray = chats.forEach((chat)=>{
-
+        console.log('chats', chats[0].user_id, user1.id);
+        console.log('chats', chats[0].guide_id, user2.id);
+        // Only send back the chats if user1 and user2 are the ones talking.
+        let filteredChats = chats.filter((chat) => {
+          return (chat.user_id === user1.id && chat.guide_id === user2.id) || (chat.user_id === user2.id && chat.guide_id === user1.id);
         });
-        var queries = [];
-        var result = [];
-
-
-        //chats.forEach(chat)
-        // console.log('seeing all chat messages',JSON.stringify(chats))
+        console.log('filteredChats', filteredChats);
         if (!chats) {
           throw chats;
         }
         if (res) {
-          res.status(200).send(chats);
+          res.status(200).send(filteredChats);
         } else {
-          callback(chats);
+          callback(filteredChats);
         }
         console.log('Successfully fetched chats!!');
       })
@@ -134,10 +71,8 @@ module.exports.getChat = (req, res, callback) => {
         console.log('error on catch', error);
         res.status(404).send([]);
       });
-      // });
     });
   })
-  // res.sendStatus(200);
     .error(err => {
       res.status(500).send(err);
     })
@@ -151,22 +86,21 @@ module.exports.getAllChatsByUser = (req, res) => {
   console.log('REQ PARAMS1', req.params);
   models.User.where({facebook_id: req.params.facebookId}).fetch({columns: ['id']})
   .then(result => {
-    models.Guide.where({user_id: result.id}).fetch({columns: ['id']})
-    .then(result2 => {
-      models.Chat.query((qb) => {
-        qb.limit(100); 
-        qb.orderBy('created_at', 'desc');
-        qb.where({user_id: result.id})
-        .orWhere({guide_id: result2.id});
-      })
-      // .fetch()
-      .then(chats => {
-        if (!chats) {
-          throw chats;
-        }
-        // res.status(200).send(chats);
-        console.log('Successfully fetched all chats for user!!', chats.length);
-      });
+    models.Chat.query((qb) => {
+      qb.limit(100); 
+      qb.orderBy('created_at', 'desc');
+      qb.where({user_id: result.id})
+      .orWhere({guide_id: result.id});
+    })
+    .fetchAll({
+      withRelated: ['user', 'guide']
+    })
+    .then(chats => {
+      if (!chats) {
+        throw chats;
+      }
+      res.status(200).send(chats);
+      console.log('Successfully fetched all chats for user!!', chats.length);
     });
   })
   .error(err => {
@@ -185,21 +119,6 @@ module.exports.getAllChatsByGuideId = (req, res) => {
     console.log('Received chats by guideId', chats);
     res.status(200).send(chats);
   })
-  // models.User.where({facebook_id: req.params.facebookId}).fetch({columns: ['id']})
-  // .then(result => {
-  //   models.Chat.query((qb) => {
-  //     qb.limit(100); 
-  //     qb.orderBy('created_at', 'desc');
-  //   })
-  //     .where({user_id: result.id}).fetchAll()
-  //     .then(chats => {
-  //       if (!chats) {
-  //         throw chats;
-  //       }
-  //       res.status(200).send(chats);
-  //       console.log('Successfully fetched all chats for user!!');
-  //     });
-  // })
     .error(err => {
       res.status(500).send(err);
     })
